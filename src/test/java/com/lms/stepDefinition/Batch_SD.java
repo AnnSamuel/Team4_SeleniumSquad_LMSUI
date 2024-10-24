392ac01e3a3121ddf5de98dd862665e15499bbc9
Pranali Dige <pranali.dige@gmail.com>
2024-10-23 23:14:53 -0500

    batch feature file started

--- src/test/java/com/lms/stepDefinition/Batch_SD.java
+++ src/test/java/com/lms/stepDefinition/Batch_SD.java
@@ -1,5 +1,27 @@
 package com.lms.stepDefinition;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class Batch_SD {
+	
+	@Given("Admin is on the Dashboard Page")
+	public void admin_is_on_the_dashboard_page() {
+	   System.out.println("Admin is on the Dashboard Page");
+	}
+
+	@When("Admin Clicks on the Batch menu from the header")
+	public void admin_clicks_on_the_batch_menu_from_the_header() {
+	   System.out.println("Admin Clicks on the Batch menu from the header");
+	}
+
+	@Then("Admin should be in the Manage Batch Page")
+	public void admin_should_be_in_the_manage_batch_page() {
+	  System.out.println("Admin should be in the Manage Batch Page");
+	}
+
+
+
 
 }
