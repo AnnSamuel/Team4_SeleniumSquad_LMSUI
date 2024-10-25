c5aa49eb9f78803cef5039e58702cceef9495677
Sandhya Sankaran <svsandhya23@gmail.com>
2024-10-25 12:05:27 -0700

    added step defs for search

--- src/test/java/com/lms/stepDefinition/Program_SD.java
+++ src/test/java/com/lms/stepDefinition/Program_SD.java
@@ -1,10 +1,13 @@
 package com.lms.stepDefinition;
 
+import java.util.ArrayList;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 
 import com.lms.driverManager.WebDriverFactory;
 import com.lms.pageObjects.ProgramPage;
+import com.lms.utilities.LoggerLoad;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -14,44 +17,75 @@ public class Program_SD {
 	
 	WebDriver driver = WebDriverFactory.getDriver();
 	ProgramPage programObj = new ProgramPage();
+	String searchText = "";
 	
-	@Given("the user is in homepage and logged in")
-	public void the_user_is_in_homepage_and_logged_in() {
+	@Given("Admin is on the Dashboard Page")
+	public void admin_is_on_the_dashboard_page() {
 		programObj.login();
-		
 	    
 	}
-
-	@When("user clicks the program button in the menu bar")
-	public void user_clicks_the_program_button_in_the_menu_bar() {
-		programObj.clickProgramBtn();
-		
-	    
-	}
-
-	@Then("user views the program page")
-	public void user_views_the_program_page() {
-		Assert.assertEquals(programObj.getProgramHeading(),"Manage Program");
-	    
-	}
-
-	@Given("the user is in program page")
-	public void the_user_is_in_program_page() {
-		programObj.login();
+	
+	@When("Admin clicks Program on the navigation bar")
+	public void admin_clicks_program_on_the_navigation_bar() {
 		programObj.clickProgramBtn();
 	    
 	}
-
-	@When("user clicks the edit button for the {string}")
-	public void user_clicks_the_edit_button_for_the(String programName) {
-		programObj.clickEditProgram(programName);
+	@Then("Admin should be navigated to Program module")
+	public void admin_should_be_navigated_to_program_module() {
+	    Assert.assertEquals(programObj.getProgramHeading(), "Manage Program");
+	}
+	
+	@Then("Admin should see {string} in menu bar")
+	public void admin_should_see_in_menu_bar(String logout) {
 	    
 	}
 
-	@Then("user views the edit program popup")
-	public void user_views_the_edit_program_popup() {
-		Assert.assertEquals(programObj.getEditProgramHeading(), "Program Details");
+	@Then("Admin should not have any broken links for Program module")
+	public void admin_should_not_have_any_broken_links_for_program_module() {
+	   
+	}
+	
+	@Then("Admin should see the heading {string}")
+	public void admin_should_see_the_heading(String string) {
 	    
+	}
+	
+	@Then("Admin should see the module names as in order")
+	public void admin_should_see_the_module_names_as_in_order() {
+	
+	}
+	
+	@Then("Admin should see sub menu in menu bar as {string}")
+	public void admin_should_see_sub_menu_in_menu_bar_as(String string) {
+	    
+	}
+	
+	//Search
+	
+	@Given("Admin is on Program module")
+	public void admin_is_on_program_module() {
+		programObj.clickProgramBtn();
+	    
+	}
+	@When("Admin enter the program to search By program details {string}")
+	public void admin_enter_the_program_to_search_by_program_details(String searchInput) {
+		
+		programObj.setSearchText(searchInput);
+		searchText = searchInput;
+		
+		
+	    
+	}
+	@Then("Admin should able to see Program name, description, and status for searched program name")
+	public void admin_should_able_to_see_program_name_description_and_status_for_searched_program_name() {
+		ArrayList<String> programNames = programObj.getProgramNames();
+		if(programObj.validateSearch(programNames,searchText)) {
+			LoggerLoad.info(programNames.size()+"");
+			LoggerLoad.info("matches");
+		}
+		else {
+			LoggerLoad.info("not matches");
+		}
 	}
 
 }
