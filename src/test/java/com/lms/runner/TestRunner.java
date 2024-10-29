e9be15a650236f2b1a20444d19c080a953923d02
sdetninjas <judefebi@gmail.com>
2024-10-29 02:33:31 -0500

    report

--- src/test/java/com/lms/runner/TestRunner.java
+++ src/test/java/com/lms/runner/TestRunner.java
@@ -5,8 +5,12 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = { "pretty",
-		"html:target/Report.html" }, monochrome = false, tags = "", features = {
+@CucumberOptions(plugin = {
+		"pretty","html:target/cucumberHtml-Report/Report.html",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+		},
+		monochrome = false, tags = "", features = {
 				"src/test/resources/features" }, glue = { "com.lms.stepDefinition", "com.lms.hooks" })
 
 public class TestRunner extends AbstractTestNGCucumberTests {
